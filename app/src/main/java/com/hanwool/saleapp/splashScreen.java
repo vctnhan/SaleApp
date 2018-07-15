f2b9072c3b3b8a50140669bc0b272da78eab1671
Hanwool <vucaotrongnhan@gmail.com>
2018-07-16 01:32:39 +0700

    Initial commit

---
+++ app/src/main/java/com/hanwool/saleapp/splashScreen.java
@@ -0,0 +1,31 @@
+package com.hanwool.saleapp;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import android.os.Handler;
+import android.view.View;
+import android.widget.ProgressBar;
+
+
+public class splashScreen extends AppCompatActivity {
+    int SPLASH_TIME_OUT = 3000;
+    ProgressBar spinner;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash_screen);
+        spinner= findViewById(R.id.spinner);
+        spinner.setVisibility(View.VISIBLE);
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent i =  new Intent(splashScreen.this, MainActivity.class);
+                startActivity(i);
+                finish();
+            }
+        }, SPLASH_TIME_OUT );
+    }
+
+}
