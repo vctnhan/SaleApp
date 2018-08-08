54beb590b5c2ed971327cffed80067558f1e2048
Hanwool <vucaotrongnhan@gmail.com>
2018-08-08 12:16:23 +0700

    update server

--- app/src/main/java/com/hanwool/saleapp/ultil/Server.java
+++ app/src/main/java/com/hanwool/saleapp/ultil/Server.java
@@ -1,6 +1,13 @@
 package com.hanwool.saleapp.ultil;
 public class Server {
-    public  static  String localhost = "10.22.208.99";
+    public  static  String localhost = "10.22.208.135";
     public  static  String DuongdanLoaisp = "http://" + localhost + "/server/getloaisp.php";
     public  static  String DuongdanSanpham = "http://" + localhost + "/server/getspmoinhat.php";
+    public  static  String DuongdanXiaomi = "http://" + localhost + "/server/getspxiaomi.php";
+    public  static  String DuongdanSamsung = "http://" + localhost + "/server/getspsamsung.php";
+    public  static  String DuongdanIphone = "http://" + localhost + "/server/getspiphone.php";
+    public  static  String DuongdanHuawei = "http://" + localhost + "/server/getsphuawei.php";
+    public  static  String DuongdanNokia = "http://" + localhost + "/server/getspnokia.php";
+    public  static  String DuongdanOppo = "http://" + localhost + "/server/getspoppo.php";
+
 }
